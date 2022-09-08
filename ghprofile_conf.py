c4973dc41f5f301f13dfc6b6112a16156c9cae15
Richard Blanchette <richardphi1618@gmail.com>
2022-09-08 16:20:11 -0400

    fix(domain): set a custom domain

--- ghprofile_conf.py
+++ ghprofile_conf.py
@@ -3,11 +3,12 @@
 
 import os
 import sys
+
 sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://richardphi1618.github.io/richardphi1618/'
+SITEURL = 'https://www.richardphi.dev'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
